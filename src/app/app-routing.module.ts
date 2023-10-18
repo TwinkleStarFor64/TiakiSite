7d76d50989d50002c9c5243670c876ed871869ce
exlineo <contact@exlineo.com>
2023-10-18 18:36:34 +0200

    Finalisation des menus et du formulaires pour les béta testeurs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { PartenairesComponent } from './pages/partenaires/partenaires.component'
 import { NewsletterComponent } from './pages/newsletter/newsletter.component';
 import { ErreurComponent } from './pages/erreur/erreur.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { CandidaterValidComponent } from './pages/candidater-valid/candidater-valid.component';
 
 const routes: Routes = [
   {path:'', component:AccueilComponent},
   {path:'mentions', component:MentionsComponent},
   {path:'testeur', component:CandidaterComponent},
-  {path:'testeur-valid', component:CandidaterComponent},
+  {path:'testeur-valid', component:CandidaterValidComponent},
   {path:'partenaires', component:PartenairesComponent},
   {path:'newsletter', component:NewsletterComponent},
   {path:'nous-contacter', component:ContactComponent},
